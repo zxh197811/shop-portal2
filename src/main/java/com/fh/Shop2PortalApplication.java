a75dfbb80ce649881d1a87801d9496c1595a970f
zhangxuhao <212145091@qq.com>
2020-01-13 17:10:00 +0800

    Initial commit

---
+++ src/main/java/com/fh/Shop2PortalApplication.java
@@ -0,0 +1,14 @@
+package com.fh;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop2PortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Shop2PortalApplication.class, args);
+    }
+
+}
